d9a8f376149a45550dd635d9a3ca565183be3e5b
vet <anthonylapeter@gmail.com>
2024-10-26 22:43:47 -0400

    Update index.js

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -1,6 +1,8 @@
 const button = document.getElementById("button")
 const header = document.getElementById("header")
 
+console.log("A");
+
 button.addEventListener("click", function() {
     console.log("You clicked the button!");
 
